c85805b8f6cbf10b1626139973dde6111a6e4304
lawson-ng <abraham.lawson.dev@gmail.com>
2024-03-17 14:49:47 +0700

    add api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:slug*",
+        destination: `http://localhost:3002/api/:slug*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const fetchAPI = async () => {
+    await fetch(`/api/users/hello`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(console.log)
+      .catch(console.error);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
   return (
     <>
       <h1>Hello Chat</h1>
     </>
-  )
+  );
 }
